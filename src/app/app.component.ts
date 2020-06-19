eada1e05a18952edf7b976c65408ca14da3b3323
nayomi16 <nayomirashmikaaa@gmail.com>
2020-06-19 22:24:18 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  showDetails = true;
+  option1 = true;
+  option2 = true;
+  option3 = true;
+  textColor = 'green';
+  inputValue = 'My name is Dasun';
+  myTitle = 'Hello Welocome to Angular 2020';
+  myAge = 30;
+  customers: string[] = ['Kamal', 'Dasun', 'Wimal', 'Ramal'];
+
+
+  getText(param) {
+    this.myTitle = param.value;
+    console.log(param.value);
+  }
+}
