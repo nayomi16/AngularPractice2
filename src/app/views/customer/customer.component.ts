e4a8c6249eb849270fbbb5e9347eab36a748c75e
nayomi16 <nayomirashmikaaa@gmail.com>
2020-06-23 20:08:12 +0530

    adding completed

--- src/app/views/customer/customer.component.ts
+++ src/app/views/customer/customer.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
-
+  id ;
+  name;
+  address;
+  customers: any[] = [];
+  cusId;
+  cusName;
+  cusAddress;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addDetails() {
+    const row = {
+      id: this.id,
+      name: this.name,
+      address: this.address
+    };
+
+    this.customers.push(row);
+  }
+  tableData(param: {id: string, name: string, address: string}) {
+    this.id = param.id;
+    this.name = param.name;
+    this.address = param.address;
+  }
+  getValues(param: any) {
+    param.id = this.cusId;
+    param.name = this.cusName;
+    param.address = this.cusAddress;
+    console.log(    param.id);
+
   }
 
 }
